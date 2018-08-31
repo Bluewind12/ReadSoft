2262487f513759b63fd17d7cd955577c652967f3
BlueWind <zero18030@gmail.com>
2018-08-31 23:58:37 +0900

    IDの変更  - IDの変更  - MainActivityのフィールドの作成

--- app/src/main/java/com/example/momonyan/readsoft/MainActivity.kt
+++ app/src/main/java/com/example/momonyan/readsoft/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.momonyan.readsoft
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var readButton: Button
+    private lateinit var optionButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
