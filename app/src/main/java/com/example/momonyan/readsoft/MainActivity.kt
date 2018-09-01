7e69122a9b55a0c3dba6cc68260fdbcefe733c41
BlueWind <zero18030@gmail.com>
2018-09-01 23:45:46 +0900

    読み上げを可能に

--- app/src/main/java/com/example/momonyan/readsoft/MainActivity.kt
+++ app/src/main/java/com/example/momonyan/readsoft/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.momonyan.readsoft
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editText: EditText
     private lateinit var readButton: Button
     private lateinit var optionButton: Button
+    private lateinit var tts: TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,11 +22,25 @@ class MainActivity : AppCompatActivity() {
         readButton = findViewById(R.id.readStartButton)
         optionButton = findViewById(R.id.optionButton)
 
+        tts = TextToSpeech(applicationContext, SampleInitListener())  //テキストトゥスピーチオブジェクトの生成と、イベントリスナーの登録
+        tts.language = Locale.JAPANESE  //読み上げる言語の設定
+
         readButton.setOnClickListener {
             val readStrings = editText.text.split("")
+            val readString = editText.text
             for (i in 0 until readStrings.size - 1) {
                 Log.d("ReadString" + i + "：", readStrings[i])
             }
+            if (tts.isSpeaking) {
+                tts.stop()
+            }
+            val utteranceId = this.hashCode().toString() + ""  //utteranceIdの取得
+            tts.speak(readString, TextToSpeech.QUEUE_FLUSH, null, utteranceId)
         }
+    }
+
+    internal inner class SampleInitListener : TextToSpeech.OnInitListener {
+        override fun onInit(status: Int) {}
+
     }
 }
